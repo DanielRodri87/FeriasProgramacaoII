e20206ee08b37114ceb11f1f1d6fd7e6e410f1ba
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-09-05 12:42:58 -0300

    att

---
+++ Danesco/daniel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("LOGIN\n");
+    char username[20], userbd[20], password[20], passbd[20];
+
+    printf("Digite o seu username: ");
+    scanf("%s", username);
+    printf("Digite a sua senha: ");
+    scanf("%s", password);
+    
+    return 0;
+}

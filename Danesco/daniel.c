81bfccc44034c3b05d6c2b983a67950d8234b45d
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-09-06 14:06:00 -0300

    teste 1

--- Danesco/daniel.c
+++ Danesco/daniel.c
@@ -1,14 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("LOGIN\n");
-    char username[20], userbd[20], password[20], passbd[20];
+    char key[20], user[20], keypad[20] = "admin", userpad[20] = "admin";
+    int op;
 
-    printf("Digite o seu username: ");
-    scanf("%s", username);
-    printf("Digite a sua senha: ");
-    scanf("%s", password);
+    do
+    {
+        system("cls");
+        printf("\n=======================================================\n");
+        printf("\n========================= LOGIN =========================\n");
+        printf("\n=======================================================\n");
+
+        printf("\nDigite uma opção: \n1 - Cliente\n2 - Funcionário\n3 - Sair -> ");
+        scanf("%d", &op);
+        
+        switch (op)
+        {
+        case 1:
+            printf("\nDigite a senha: ");
+            scanf("%s", key);
+            if (strcmp(key, keypad) == 0)
+            {
+                printf("\nLogin efetuado com sucesso!\n");
+                system("pause");
+            }
+            else
+            {
+                printf("\nSenha incorreta!\n");
+                system("pause");
+            }
+            break;
+        case 2:
+            printf("\nDigite o usuário: ");
+            scanf("%s", user);
+            printf("\nDigite a senha: ");
+            scanf("%s", key);
+            if (strcmp(user, userpad) == 0 && strcmp(key, keypad) == 0)
+            {
+                printf("\nLogin efetuado com sucesso!\n");
+                system("pause");
+            }
+            else
+            {
+                printf("\nUsuário ou senha incorretos!\n");
+                system("pause");
+            }
+            break;
+
+        default:
+            printf("\nOpção inválida!\n");
+            system("pause");
+            break;
+        }
     
+
+    } while (op!=3);
     return 0;
 }
